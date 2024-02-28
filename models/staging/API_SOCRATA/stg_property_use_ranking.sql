126acfb5372a0b8f0b13b91ac85ba1cf1de04730
Julia Rivera <juliarivera@correo.ugr.es>
2024-02-28 16:43:20 +0000

    commit cambios

--- models/staging/API_SOCRATA/stg_property_use_ranking.sql
+++ models/staging/API_SOCRATA/stg_property_use_ranking.sql
@@ -20,9 +20,13 @@ with src_property_use_ranking as (
             then null
             else taxparcelidentificationnumber
         end as id_property,
-        trim(largestpropertyusetype) as property_use_type,
+        trim(largestpropertyusetype)::varchar(100) as property_use_type,
         1 as property_use_ranking,
-        largestpropertyusetypegfa as property_use_type_gfa
+        case 
+            when largestpropertyusetypegfa = 'NA' then null 
+            when try_to_number(largestpropertyusetypegfa) is null then null 
+            else largestpropertyusetypegfa::float
+        end as property_use_type_gfa,
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where largestpropertyusetype is not null and trim(largestpropertyusetype) <> ''
 
@@ -35,9 +39,13 @@ with src_property_use_ranking as (
             then null
             else taxparcelidentificationnumber
         end as id_property,
-        trim(secondlargestpropertyusetype) as property_use_type,
+        trim(secondlargestpropertyusetype)::varchar(100) as property_use_type,
         2 as property_use_ranking,
-        secondlargestpropertyuse as property_use_type_gfa
+        case 
+            when secondlargestpropertyuse = 'NA' then null 
+            when try_to_number(secondlargestpropertyuse) is null then null 
+            else secondlargestpropertyuse::float
+        end as property_use_type_gfa,
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where secondlargestpropertyusetype is not null and trim(secondlargestpropertyusetype) <> ''
 
@@ -50,9 +58,13 @@ with src_property_use_ranking as (
             then null
             else taxparcelidentificationnumber
         end as id_property,
-        trim(thirdlargestpropertyusetype) as property_use_type,
+        trim(thirdlargestpropertyusetype)::varchar(100) as property_use_type,
         3 as property_use_ranking,
-        thirdlargestpropertyusetypegfa as property_use_type_gfa
+        case 
+            when thirdlargestpropertyusetypegfa = 'NA' then null 
+            when try_to_number(thirdlargestpropertyusetypegfa) is null then null 
+            else thirdlargestpropertyusetypegfa::float
+        end as property_use_type_gfa
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where thirdlargestpropertyusetype is not null and trim(thirdlargestpropertyusetype) <> ''
 ),
--- models/staging/API_SOCRATA/stg_property_use_type.sql
+++ models/staging/API_SOCRATA/stg_property_use_type.sql
@@ -12,53 +12,71 @@
 }}
 
 with distinct_property_types as (
-    select distinct
-        trim(largestpropertyusetype)::varchar(100) as property_use_type
+    select
+        case
+             when largestpropertyusetype = 'NA' then null
+             else trim(largestpropertyusetype)::varchar(100) 
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where largestpropertyusetype is not null and largestpropertyusetype != ''
 
     union all
 
-    select distinct
-        trim(secondlargestpropertyusetype)::varchar(100) as property_use_type
+    select
+        case
+            when secondlargestpropertyusetype = 'NA' then null
+            else trim(secondlargestpropertyusetype)::varchar(100) 
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where secondlargestpropertyusetype is not null and secondlargestpropertyusetype != ''
 
     union all
 
-    select distinct
-        trim(thirdlargestpropertyusetype)::varchar(100) as property_use_type
+    select
+        case 
+            when thirdlargestpropertyusetype = 'NA' then null
+            else trim(thirdlargestpropertyusetype)::varchar(100) 
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where thirdlargestpropertyusetype is not null and thirdlargestpropertyusetype != ''
 
     union all
         
-    select distinct
-        trim(epapropertytype)::varchar(100) as property_use_type
+    select
+        case
+            when epapropertytype = 'NA' then null
+            else trim(epapropertytype)::varchar(100) 
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where epapropertytype is not null and epapropertytype != ''
 
     union all
         
-    select distinct
-        trim(primarypropertytype)::varchar(100) as property_use_type
+    select
+        case
+            when primarypropertytype = 'NA' then null
+            else trim(primarypropertytype)::varchar(100)
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }}
     where primarypropertytype is not null and primarypropertytype != ''  
 
     union all
 
-    select distinct
-        trim(value)::varchar(100) as property_use_type
+    select
+        case
+            when value = 'NA' then null
+            else trim(value)::varchar(100)
+        end as property_use_type
     from {{ source('api_socrata', 'building_energy_benchmarking') }},
     lateral split_to_table(listofallpropertyusetypes, ',') as split
     where split.value is not null and trim(split.value) <> ''
 ),
 
 stg_property_use_type as (
-    select distinct
+    select
         {{ dbt_utils.generate_surrogate_key(['property_use_type']) }} as id_property_use_type
         , property_use_type
     from distinct_property_types
 )
 
-select * from stg_property_use_type
+select distinct * from stg_property_use_type
